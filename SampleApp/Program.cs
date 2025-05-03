cc7df594928a7ed5a2f50f7998dbb845ada8d25e
Ram.Type-0 <p0211@live.jp>
2025-05-04 06:13:24 +0900

    Update Kroki

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -9,5 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddKrokiClient();
+builder.Services.AddKrokiHttpRequestFactory();
 await builder.Build().RunAsync();
