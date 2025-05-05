d91380ba95c1c72ab4715fa5bbb35d72dc3cbe8c
Ram.Type-0 <p0211@live.jp>
2025-05-06 02:34:56 +0900

    Update Sample App

---
+++ Sample/SampleApp.Web/SampleApp.Web.Client/Pages/Home.razor.cs
@@ -0,0 +1,31 @@
+﻿using Markdig;
+using Markdig.Renderers.MudBlazor;
+using Markdig.Renderers.RazorComponent.Katex;
+using Markdig.Renderers.RazorComponent.Kroki;
+using Markdig.Renderers.RazorComponent.Vega.Embed;
+
+namespace SampleApp.Web.Client.Pages;
+
+partial class Home
+{
+    static MarkdownPipeline MarkdownPipeline { get; } = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseMudBlazor().UseVegaEmbed().UseKatex().Build();
+    static readonly string markdownText = """
+### Markdig.Renderers.RazorComponent Sample App
+
+**Welcome to sample app!**
+
+You can experience 
+  - Live Markdown Preview
+  - Ollama chat with markdown formatted response
+
+with this sample.
+
+This page demonstrates SSR with this library.
+
+SSR is also supported for
+
+$KaTeX math inlines$.
+
+
+""";
+}
