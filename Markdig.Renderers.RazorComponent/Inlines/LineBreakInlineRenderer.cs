f7ada0699116359032e377c42fa66b595c3e6f46
Ram.Type-0 <p0211@live.jp>
2025-05-10 17:53:54 +0900

    Fix `LineBreakInline` is always rendered as hard line break

--- Markdig.Renderers.RazorComponent/Inlines/LineBreakInlineRenderer.cs
+++ Markdig.Renderers.RazorComponent/Inlines/LineBreakInlineRenderer.cs
@@ -4,13 +4,20 @@ namespace Markdig.Renderers.RazorComponent.Inlines;
 
 public class LineBreakInlineRenderer : RazorComponentObjectRenderer<LineBreakInline>
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether to render this softline break as a HTML hardline break tag (&lt;br /&gt;)
+    /// </summary>
+    public bool RenderAsHardlineBreak { get; set; }
     protected override void Write(RazorComponentRenderer renderer, LineBreakInline obj)
     {
         var builder = renderer.Builder;
-        builder.OpenElement(0, "br");
+        if (obj.IsHard || RenderAsHardlineBreak)
         {
+            builder.OpenElement(0, "br");
+            {
 
+            }
+            builder.CloseElement();
         }
-        builder.CloseElement();
     }
 }
