c958f927df0bd6599d2e5b1273eb03c908dc5837
Ram.Type-0 <p0211@live.jp>
2026-02-18 11:44:51 +0900

    Update sample

--- Sample/SampleApp.Web/SampleApp.Web.Client/Pages/MarkdownEditor.razor.cs
+++ Sample/SampleApp.Web/SampleApp.Web.Client/Pages/MarkdownEditor.razor.cs
@@ -19,6 +19,21 @@ Paragraph
 
 This is a $math inline$
 
+> [!NOTE]
+> Highlights information that users should take into account, even when skimming.
+
+> [!TIP]
+> Optional information to help a user be more successful.
+
+> [!IMPORTANT]
+> Crucial information necessary for users to succeed.
+
+> [!WARNING]
+> Critical content demanding immediate user attention due to potential risks.
+
+> [!CAUTION]
+> Negative potential consequences of an action.
+
  a     | b
 -------|-------
  0     | 1
